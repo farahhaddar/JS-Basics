b27a40ecf2715d5343d14f209cb37941d4eec01b
farahhaddar <farah.haddar99@gmail.com>
2020-09-28 13:19:46 +0300

    B1 js step 4

---
+++ javascript-basics-01/step_4.js
@@ -0,0 +1,5 @@
+var name,surname,city;
+name= prompt("Enter you name");
+surname= prompt("Enter you surname");
+city= prompt("Enter which city are you from");
+alert(" hello,"+name+"  "+surname+"  "+"from"+city+"!");
