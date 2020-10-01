ddc562e37b938438e1ffe66e303a126b93c72e0f
farahhaddar <farah.haddar99@gmail.com>
2020-10-01 11:18:51 +0300

    B3 step 3

---
+++ javascript-basics-03/step 3/step 3.js
@@ -0,0 +1,17 @@
+
+var green = document.querySelector(".green");
+var Red = document.querySelector('.red');
+var Blue = document.querySelector('.blue');
+var text = document.getElementById("text");
+
+green.addEventListener("click",function(){
+    text.style.color = "green";
+})
+
+Red.addEventListener("click",function(){
+    text.style.color = "red";
+})
+
+Blue.addEventListener("click",function(){
+    text.style.color = "blue";
+})
